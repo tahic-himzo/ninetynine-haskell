02a8ea5f2d423531ab2119665b26f04513948ef0
Himzo Tahic <himzotahic@gmail.com>
2020-03-29 09:47:16 +0200

    add NEL implementation

--- src/Lists/MyLast.hs
+++ src/Lists/MyLast.hs
@@ -1,5 +1,12 @@
-module Lists.MyLast (myLast) where
+module Lists.MyLast (myLast, myLastNonEmpty) where
+    import Data.List.NonEmpty
+
     myLast :: [a] -> Maybe a
+    myLastNonEmpty :: NonEmpty a -> a
+
     myLast []     = Nothing
     myLast [x]    = Just x
     myLast (_:xs) = myLast xs
+    
+    myLastNonEmpty (_ :| (b : as)) = myLastNonEmpty (b :| as)
+    myLastNonEmpty (a :| []) = a
--- test/Lists/MyLastSpec.hs
+++ test/Lists/MyLastSpec.hs
@@ -1,14 +1,25 @@
 module Lists.MyLastSpec where
 
+import           Data.List.NonEmpty
 import           Lists.MyLast
 import           Test.Hspec
 import           Test.QuickCheck
 
 spec :: Spec
-spec =
+spec = do
   describe "Lists.MyLast" $ do
-    it "returns the last element of a list" $
+    it "returns the last element of a long list" $
       myLast [1,2,3] `shouldBe` Just 3
+
+    it "returns the last element of a short list" $
+      myLast [1] `shouldBe` Just 1
 
     it "return Nothing if used with an empty list" $
       myLast ([] :: [Int]) `shouldBe` (Nothing :: Maybe Int)
+
+  describe "Lists.MyLastNonEmpty" $ do
+    it "returns the last element of a long NEL" $
+      myLastNonEmpty (1 :| [2, 3]) `shouldBe` 3
+
+    it "returns the last element of a short NEL" $
+      myLastNonEmpty (1 :| []) `shouldBe` 1
