db6116f26854b74be770572ea1ad764566444224
Himzo Tahic <himzotahic@gmail.com>
2020-03-29 11:18:28 +0200

    Update ElementAt.hs

--- src/Lists/ElementAt.hs
+++ src/Lists/ElementAt.hs
@@ -1,10 +1,10 @@
 module Lists.ElementAt (elementAt) where
   elementAt :: [a] -> Int -> Maybe a
 
+  elementAt [] _ = Nothing
   elementAt (x : _) 1 = Just x
   elementAt (_ : xs) i
    | i > 1 = elementAt xs (i-1)
    | otherwise = Nothing
 
-  elementAt [] i = Nothing
 
