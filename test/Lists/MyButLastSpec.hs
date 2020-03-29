c453c4fd343951b9c6c8b61a16b49ad4ed8933b9
Himzo Tahic <himzotahic@gmail.com>
2020-03-29 10:15:22 +0200

    Update MyButLastSpec.hs

--- test/Lists/MyButLastSpec.hs
+++ test/Lists/MyButLastSpec.hs
@@ -6,6 +6,10 @@ import           Test.QuickCheck
 
 spec :: Spec
 spec =
-  describe "Lists.MyButLast" $
+  describe "Lists.MyButLast" $ do
       it "returns the penultimate item of a list" $
-          myButLast [1,2,3] `shouldBe` Just 2
+        myButLast [1,2,3] `shouldBe` Just 2
+
+      it "returns nothing for a list with 1 or less items" $ do
+        myButLast ([] :: [Int]) `shouldBe` Nothing
+        myButLast [1] `shouldBe` Nothing
